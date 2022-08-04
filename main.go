46df993091799240503402bca52627a65327f386
Flokey82 <f.rohrweck@gmail.com>
2022-08-04 17:39:45 +0200

    Game of life

--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"log"
+
+	"github.com/Flokey82/go_gens/gencellular"
 	"github.com/hajimehoshi/ebiten"
 )
 
+const (
+	height = 128
+	width  = 128
+)
+
 func main() {
+	g := newGame()
 	ebiten.SetFullscreen(true)
-	//ebiten.SetWindowSize(dungeon.ScreenWidth*10, dungeon.ScreenHeight*10)
+	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("Dungeon Crawler")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct {
+	*gencellular.Culture
+	pixels []byte
+}
+
+func newGame() *Game {
+	return &Game{
+		Culture: gencellular.New(width, height),
+	}
+}
+
+// Update game state by one tick.
+func (w *Game) Update() error {
+	w.Culture.Tick()
+	return nil
+}
+
+func (w *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return width, height
+}
+
+// Draw paints current game state.
+func (w *Game) Draw(screen *ebiten.Image) {
+	if w.pixels == nil {
+		w.pixels = make([]byte, width*height*4)
+	}
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			if w.Cells[(w.Generation+1)%2][x][y] {
+				w.pixels[(y*width+x)*4] = 0xff
+				w.pixels[(y*width+x)*4+1] = 0xff
+				w.pixels[(y*width+x)*4+2] = 0xff
+				w.pixels[(y*width+x)*4+3] = 0xff
+			} else {
+				w.pixels[(y*width+x)*4] = 0x00
+				w.pixels[(y*width+x)*4+1] = 0x00
+				w.pixels[(y*width+x)*4+2] = 0x00
+				w.pixels[(y*width+x)*4+3] = 0x00
+			}
+		}
+	}
 }

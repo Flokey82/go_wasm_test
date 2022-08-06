df02a9d228c5691e1c38deba343dd27130722be3
Flokey82 <f.rohrweck@gmail.com>
2022-08-06 12:04:17 +0200

    Add reset on "r" and FPS counter

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
 	"log"
+	"math/rand"
+	"time"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 
 	"github.com/Flokey82/go_gens/gencellular"
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/text"
 )
 
 const (
-	height = 128
-	width  = 128
+	height = 512
+	width  = 512
 )
+
+const sampleText = `The quick brown fox jumps over the lazy dog.`
+
+var (
+	mplusNormalFont font.Face
+)
+
+func init() {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    14,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	g := newGame()
@@ -34,6 +66,10 @@ func newGame() *Game {
 
 // Update game state by one tick.
 func (w *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyR) {
+		rand.Seed(time.Now().UnixNano())
+		w.Culture.Reset()
+	}
 	w.Culture.Tick()
 	return nil
 }
@@ -48,20 +84,24 @@ func (w *Game) Draw(screen *ebiten.Image) {
 		w.pixels = make([]byte, width*height*4)
 	}
 
+	var col byte
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			if w.Cells[(w.Generation+1)%2][x][y] {
-				w.pixels[(y*width+x)*4] = 0xff
-				w.pixels[(y*width+x)*4+1] = 0xff
-				w.pixels[(y*width+x)*4+2] = 0xff
-				w.pixels[(y*width+x)*4+3] = 0xff
+				col = 0xFF
 			} else {
-				w.pixels[(y*width+x)*4] = 0x00
-				w.pixels[(y*width+x)*4+1] = 0x00
-				w.pixels[(y*width+x)*4+2] = 0x00
-				w.pixels[(y*width+x)*4+3] = 0x00
+				col = 0x00
 			}
+			w.pixels[(y*width+x)*4] = col
+			w.pixels[(y*width+x)*4+1] = col
+			w.pixels[(y*width+x)*4+2] = col
+			w.pixels[(y*width+x)*4+3] = col
 		}
 	}
 	screen.ReplacePixels(w.pixels)
+	// Draw info
+	const x = 2
+	msg := fmt.Sprintf("TPS: %0.2f", ebiten.ActualTPS())
+	text.Draw(screen, msg, mplusNormalFont, x, 17, color.White)
+	text.Draw(screen, "Press R to reset", mplusNormalFont, x, 17+17, color.White)
 }
